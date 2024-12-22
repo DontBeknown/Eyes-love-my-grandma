e8b6dd0ef1fcf6048f3c396028f22dc78eaa18a9
Rozene Mokem <Enezor0612@gmail.com>
2024-12-22 18:06:49 +0700

    Add Project file

---
+++ Eye Love My Grandma Unity/Assets/Script/Changescene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.VR;
+
+public class Changescene : MonoBehaviour
+{
+    public void Goto2D()
+    {
+        SceneManager.LoadScene("2Dscene");
+    }
+
+    public void GotoVR()
+    {
+        SceneManager.LoadScene("3DVrScene");
+    }
+}
